e31ec164e36bb2fdfe33f5840594a4bafb88b33c
Alberto <simoalgipc20@gmail.com>
2022-01-25 02:22:02 +0100

    mapas2 commit

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,17 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+const mapboxgl = require('mapbox-gl');
+
+mapboxgl.accessToken = 'pk.eyJ1IjoicHNpbWFycm83IiwiYSI6ImNreWluY3I5YjF4NTUydnFoN2U4cTd1MjcifQ.f9g-9fuJpgqnzKqLsoRKdw';
+
+if ( !navigator.geolocation ) {
+  alert('Navegador no soporta la Geolocation');
+  throw new Error('Navegador no soporta la Geolocation');
+}
+
+
+
 if (environment.production) {
   enableProdMode();
 }
